2655147783d444650241942382c759c1a3ea4699
BokanidjaTeKero <bojanpetrovic308@yahoo.com>
2020-04-19 23:30:58 +0200

    The Search and Filter component is done, and now the functionality

--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -19,14 +19,104 @@ render() {
                 <button className="btn-floating btn-small waves-effect waves-light light-green btn"><i className="material-icons">settings</i></button>
                 <button className="btn-floating btn-small waves-effect waves-light light-green btn"><i className="material-icons">search</i></button>
             </div>
-            <form className='filter-form' action='#'>
-                <p>
-                <label>
-                    <input id="indeterminate-checkbox" type="checkbox" />
-                    <span>Indeterminate Style</span>
-                </label>
-                </p>
-            </form>
+            <div className='forms-container'>
+                <div className='forms-contents'>
+                    {/* <div className='calories-element'>
+                        <p>Calories</p>
+                        <form className='calories-form' action='#'>
+                            <label>
+                                <span>From</span>
+                                <input className="calories-input" type="number" />
+                            </label>
+                            <label>
+                                <span>To</span>
+                                <input className="calories-input" type="number" />
+                            </label>
+                        </form>
+                    </div> */}
+                    <div className='forms-content'>
+
+                    <div className='calories-element'>
+                        <p>Calories</p>
+                        <form className='calories-form' action='#'>
+                            <label>
+                                <span>From</span>
+                                <input className="calories-input" type="number" />
+                            </label>
+                            <label>
+                                <span>To</span>
+                                <input className="calories-input" type="number" />
+                            </label>
+                        </form>
+                    </div>
+
+
+                        <div className='health-element form-chbox'>
+                            <p>Health</p>
+                            <form className='filter-form health-form' action='#'>
+                                <p>
+                                <label>
+                                    <input id="indeterminate-checkbox" type="checkbox" />
+                                    <span>Alcohol free</span>
+                                </label>
+                                </p>
+                                <p>
+                                <label>
+                                    <input id="indeterminate-checkbox" type="checkbox" />
+                                    <span>Vegan</span>
+                                </label>
+                                </p>
+                                <p>
+                                <label>
+                                    <input id="indeterminate-checkbox" type="checkbox" />
+                                    <span>Vegetarian</span>
+                                </label>
+                                </p>
+                                <p>
+                                <label>
+                                    <input id="indeterminate-checkbox" type="checkbox" />
+                                    <span>Peanuts</span>
+                                </label>
+                                </p>
+                            </form>
+                        </div>
+                        <div className='health-element form-chbox'>
+                            <p>Diet</p>
+                            <form className='filter-form diet-form' action='#'>
+                                <p>
+                                <label>
+                                    <input id="indeterminate-checkbox" type="checkbox" />
+                                    <span>Balanced</span>
+                                </label>
+                                </p>
+                                <p>
+                                <label>
+                                    <input id="indeterminate-checkbox" type="checkbox" />
+                                    <span>High-Protein</span>
+                                </label>
+                                </p>
+                                <p>
+                                <label>
+                                    <input id="indeterminate-checkbox" type="checkbox" />
+                                    <span>Low-Carb</span>
+                                </label>
+                                </p>
+                                <p>
+                                <label>
+                                    <input id="indeterminate-checkbox" type="checkbox" />
+                                    <span>Low-Fat</span>
+                                </label>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div className='forms-controls'>
+                    <button className="btn-floating btn-small waves-effect waves-light light-green btn"><i className="material-icons">refresh</i></button>
+                    <button className="btn-floating btn-small waves-effect waves-light light-green btn"><i className="material-icons">done</i></button>
+                    <button className="btn-floating btn-small waves-effect waves-light light-green btn"><i className="material-icons">close</i></button>
+                </div>
+            </div>
         </div>
         )
     }
