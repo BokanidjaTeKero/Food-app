16fc033ac4588776c22669b3b1f945094e7a65f9
BokanidjaTeKero <bojanpetrovic308@yahoo.com>
2020-05-25 07:57:56 +0200

    Updates

---
+++ precache-manifest.e313ea76770d750fb27d861007694319.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10be2dd24c405014af7af4d118ce583d",
+    "url": "/Food-app/index.html"
+  },
+  {
+    "revision": "795cb3291d6bfc3a0a00",
+    "url": "/Food-app/static/css/main.f959174d.chunk.css"
+  },
+  {
+    "revision": "223994e8aa190452925d",
+    "url": "/Food-app/static/js/2.ed2fc4f7.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/Food-app/static/js/2.ed2fc4f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795cb3291d6bfc3a0a00",
+    "url": "/Food-app/static/js/main.a82f10fc.chunk.js"
+  },
+  {
+    "revision": "0cefb466b7bc471050a3",
+    "url": "/Food-app/static/js/runtime-main.863c9489.js"
+  },
+  {
+    "revision": "8e708110aeee0cf1e061101756ac835b",
+    "url": "/Food-app/static/media/background.8e708110.jpg"
+  },
+  {
+    "revision": "4aafd5506435757b3fb0985414b9f1ef",
+    "url": "/Food-app/static/media/loader3.4aafd550.gif"
+  },
+  {
+    "revision": "c334adabee75bd398320e72ad1539213",
+    "url": "/Food-app/static/media/loader3mini.c334adab.gif"
+  },
+  {
+    "revision": "1abf65949b799080859d7bba27782e92",
+    "url": "/Food-app/static/media/modalBg1.1abf6594.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Food-app/precache-manifest.e313ea76770d750fb27d861007694319.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Food-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
